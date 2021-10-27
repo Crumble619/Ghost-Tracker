310956205bf034a47846d1ddfbeb5eadb11eb3c8
Colin Greene <colinpatrickgreene@gmail.com>
2021-10-27 13:12:11 -0700

    Initial Commit

---
+++ Ghost Tracker/Ghost_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ghost_TrackerApp.swift
+//  Ghost Tracker
+//
+//  Created by Colin Greene on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Ghost_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
